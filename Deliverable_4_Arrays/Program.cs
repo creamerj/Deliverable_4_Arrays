966350bbe2fdb22212570bc3bea05a2b2f7a8fcb
Jessica <Jessica@DESKTOP-8TV32BN>
2023-02-07 01:25:56 -0500

    Add project files.

---
+++ Deliverable_4_Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Author: Jessica C
+ * Date: 2/7/2023
+ * Description: Fib. sequence displayed.
+ * */
+
+namespace Deliverable_4_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fib = new int[25];
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for (int i = 0; i < fib.Length; i++)
+            {
+                if (i >= 2)
+                {
+                    fib[i] = fib[i - 1] + fib[i - 2];
+
+                }
+                Console.WriteLine("Fibonacci number (" + i + ") = " + fib[i]);
+            }
+
+        }
+    }
+}
